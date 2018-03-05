33c1268fb156620c1d09b784b59330cad3176483
Yoshud <adam0cisak@gmail.com>
2018-03-05 21:57:49 +0100

    W trakcie 7 cz.

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Question
 # Register your mfrom .models import Question
+from django.contrib import admin
 
-admin.site.register(Question)
+from .models import Question, Choices
+
+class ChoiceInline(admin.TabularInline):
+    model = Choices
+    extra = 3
+   # fields = ['choice_text']
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date', 'choices__votes']
+    search_fields = ['question_text']
+    date_hierarchy = 'pub_date'
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choices)
--- polls/models.py
+++ polls/models.py
@@ -9,6 +9,9 @@ class Question(models.Model):
     def was_published_recently(self):
         return (self.pub_date >= timezone.now() - datetime.timedelta(days = 1) and self.pub_date <= timezone.now() )
 
+    was_published_recently.admin_order_field = 'pub_date' #daje mozliwosc sortowanie po paramaetrzy pub date, domysnie nie sortuje - nie jest wspierane
+    #was_published_recently.boolean = True #zmienia na znaczki (dodaje sposob pokazywania dla boolean)
+    was_published_recently.short_description = 'Published recently?'  #dodaje nazwe
 class Choices(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
